2a1a794c31e15f6d12fd44b209488259c10b97b2
lucas-fochesatto <lucas.fochesatto@gmail.com>
2024-04-06 16:58:45 -0300

    cors

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const app = express()
 app.use(express.json())
 app.use(cors({origin: '*'}))
 
+app.use((err, req, res, next) => {
+    console.log(err.stack)
+    res.status(500).send("Something broke!")
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+})
+
 app.get('/Managers', async (req, res) => {
     const Managers = await getManagers()
     res.send(Managers)
@@ -18,16 +25,10 @@ app.get('/Managers/:id', async (req, res) => {
     res.send(note)
 })
 
-app.post('/addManager', async (req, res) => {
+app.get('/addManager', async (req, res) => {
     const { manager, building } = req.body
     const property = await addManager(manager, building)
     res.status(201).send(property)
-})
-
-app.use((err, req, res, next) => {
-    console.log(err.stack)
-    res.status(500).send("Something broke!")
-    res.setHeader('Access-Control-Allow-Origin', '*');
 })
 
 app.listen(8080, () => {

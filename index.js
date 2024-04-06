ac37d057ea4f8c702ff54995b8bfbd6aa7daf869
lucas-fochesatto <lucas.fochesatto@gmail.com>
2024-04-06 16:09:45 -0300

    cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import { getManagers, getManager, addManager} from './database.js'
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.get('/Managers', async (req, res) => {
     const Managers = await getManagers()
